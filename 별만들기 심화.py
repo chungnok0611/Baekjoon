b6aba9222d31e326f579dc006ba53f18d97aef4f
chungnok0611 <chungnok0611@gmail.com>
2022-10-14 23:54:19 +0900

    별출력 심화

---
+++ 별만들기 심화.py
@@ -0,0 +1,14 @@
+n = 10
+m=1
+for i in range (1,10):
+    m=m+1
+    print(end="\n")
+    for j in range (1,m):
+        print(end="* ")
+
+for i in range (1,10):
+    n=n-1
+    print(end="\n")
+    for j in range (1,n):
+        print(end="* ")
+        
