2eec33679f23043d2101990c38351ef7ed44e3cd
chungnok0611 <chungnok0611@gmail.com>
2022-10-16 23:40:40 +0900

    하노이

---
+++ 하노이.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void hanoi(int n, char start, char mid, char end) {
+	if (n == 0) return ;//종료하기
+	hanoi(n - 1, start, end, mid);
+	printf("%d : %c -> %c\n", n, start, end);
+	hanoi(n - 1, mid, start, end);
+}
+int main() {
+	int n;
+	scanf("%d", &n);
+	hanoi(n, 'A', 'B', 'C');//함수호출시 매개변수는 n, 'A', 'B', 'C'을 활용한다.
+	return 0;
+}
