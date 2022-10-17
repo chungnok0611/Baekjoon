d0050f8bf15fb4fd258772198faf74eec3c262a9
chungnok0611 <chungnok0611@gmail.com>
2022-10-17 23:34:54 +0900

    버블정렬

--- 정렬.py
+++ 정렬.py
@@ -1,12 +1,13 @@
-a=list(243,231,23,12,23,24)
+a=[1, 3, 5, 7, 9,2,4,6,8]
 n=0
 j = -1
-for i in a:
-    for j in a:
-        if a[j]>a[i]:
+for i in range(1,10):
+    for j in range(1,10):
+        
+        if a[1]<a[2]:
             a[j]=n
             a[j]=a[i]
             a[i]=n
 
-for k in a:
+for k in range(1,10):
     print(k)
